a6d2c373bda1f6ab1059a74caf6301e9f0592f17
--SHADOW-- <--SHADOW--@DESKTOP-BECBC03>
2024-10-10 08:22:04 -0300

    creacion de carpeta raiz-Chincha Luis Miguel

---
+++ src/main/java/ar/edu/ies6/Tp07Grupo1Ies6Application.java
@@ -0,0 +1,13 @@
+package ar.edu.ies6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp07Grupo1Ies6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp07Grupo1Ies6Application.class, args);
+	}
+
+}
